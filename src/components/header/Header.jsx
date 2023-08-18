0e3bc081dfc7e12c6c2462fdfd09f1640ca763cf
vikram <vikrampchoudhary777@gmail.com>
2023-08-18 22:07:49 +0530

    image add

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import Me from "../../asset/me.png";
+import Me from "../../asset/vikram.jpg";
 import HeaderSocial from './HeaderSocials'
 const Header = () => {
   return (
