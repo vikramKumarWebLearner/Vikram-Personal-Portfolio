e36649ab78773da8b9b087a3fe83317a9b1a4bb6
vikram <vikrampchoudhary777@gmail.com>
2023-10-16 09:13:47 +0530

    photo update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import Me from "../../asset/me-about.jpg";
+import Me from "../../asset/vikram-about.png";
 import {FaAward} from 'react-icons/fa'
 import {FiUsers}  from 'react-icons/fi'
 import {VscFolderLibrary} from 'react-icons/vsc'
@@ -13,7 +13,7 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={Me} alt="about Image" />
+            <img src={Me} alt="about Image"  />
           </div>
         </div>
       
